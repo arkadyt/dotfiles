0a75e6d76b65a640e330bad7bf3ef836a224b341
arkadyt <contact.me@arkadyt.dev>
2020-01-01 14:10:48 -0800

    Rewrite cert renewal script

--- cloud/api-servers/foss-other/renew-ssl-certs.sh
+++ cloud/api-servers/foss-other/renew-ssl-certs.sh
@@ -4,23 +4,58 @@
 #     sudo su -
 #     run-parts /etc/cron.*
 
-export TZ='America/Los_Angeles'
-
-DATE=$(date +'%B %d, %Y')
-DATE_PRECISE=$(date +'%B %d, %Y | %H:%M:%S (%z)')
-
-ATTACHMENT="/home/arkadyt/cron.reports/cert_renewal_report_$(date +'%Y%m%d_%H%M%S').txt"
+LOCAL_REPORTS_DIR=/home/ubuntu/cron.reports
 EMAIL='redacted@arkadyt.com'
 
-HEADER="Weekly cert update report ($DATE)"
-OUTPUT=$(certbot renew -n \
-         --pre-hook  "service nginx stop" \
-         --post-hook "service nginx start" )
+export TZ='America/Los_Angeles'
+mkdir -p $LOCAL_REPORTS_DIR
 
-BODY="DATE: $DATE_PRECISE
-COMMAND: certbot renew (with pre & post hooks that restart nginx)
-WHERE: $(hostname -f)
-REPORT: $OUTPUT"
+function write_to_file {
+  local pathname=$1; shift
+  local contents=("$@")
 
-echo "$BODY" >> $ATTACHMENT
-echo "" | mail -s "$HEADER" -A $ATTACHMENT $EMAIL
+  if [ -f $pathname ]; then 
+    truncate -s 0 $pathname
+  else
+    touch $pathname
+  fi
+
+  echo ${contents[@]} | sed "s/ /\n/g" > $pathname
+}
+
+function compose_message {
+  local date_exact=$(date +'%B %d, %Y | %H:%M:%S (%z)')
+  local report=$(certbot renew -n \
+           --pre-hook  "service nginx stop" \
+           --post-hook "service nginx start" )
+
+  local message=(
+    "Timestamp: $date_exact"
+    "Cmd: certbot renew (w/ nginx restart)"
+    "Server: $(hostname -f)"
+    "Cmd result:"
+    "-----------"
+    "$report"
+  )
+
+  return $message
+}
+
+function save_local_copy {
+  local filename="$LOCAL_REPORTS_DIR/cert_renewal_report_$(date +'%Y%m%d_%H%M%S').txt"
+  write_to_file $filename $1
+}
+
+function send_email {
+  local date=$(date +'%B %d, %Y')
+  local header="Weekly cert update report ($date)"
+  echo "" | mail -s "$header" -A $1 $EMAIL
+}
+
+function renew_certs {
+  compose_message; content_arr=$?
+  save_local_copy $content_arr
+  # send_email $content_arr
+}
+
+renew_certs
