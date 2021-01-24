cfea134c04a7a0bd87a5bb347d9915964f4acf6e
arkadyt <contact.me@arkadyt.dev>
2021-01-24 13:31:47 -0600

    Add setup script

---
+++ workstation/setup/main.sh
@@ -0,0 +1,22 @@
+# 
+# Environment setup script.
+# Expects dotfiles repo to be located at $HOME/code/dotfiles
+#
+
+sudo apt install -y tree neovim git htop
+
+# install bash aliases link
+SOURCE=$HOME/code/dotfiles/workstation/bash/.bash_aliases
+TARGET=$HOME/.bash_aliases
+if [[ -f "$TARGET" ]]; then
+	mv $TARGET $TARGET.bkp
+fi
+ln -s $SOURCE $TARGET
+
+# install neovim config
+SOURCE=$HOME/code/dotfiles/workstation/nvim/init.vim
+TARGET=$HOME/.config/nvim/init.vim
+if [[ -f "TARGET" ]]; then
+	mv $TARGET $TARGET.bkp
+fi
+ln -s $SOURCE $TARGET 
